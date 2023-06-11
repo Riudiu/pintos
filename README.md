ad24dfe17241ccc707ea218b8ff816adb44b5155
Riudiu <lacvert13@gmail.com>
2023-06-12 01:09:07 +0900

    Revert "Implement Argument Passing"
    This reverts commit 9b7bcf42ef64a608f0d8bd7ef5d13b5e28001041.

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ AWS EC2 Ubuntu 18.04 (x86_64)
     $ sudo apt update
     $ sudo apt install -y gcc make qemu-system-x86 python3
 
-## PJ1. Threads
+## CH1. Alarm clock, Priority Scheduling
 
 제대로 동작되는지 테스트 결과 확인
 
@@ -23,9 +23,8 @@ AWS EC2 Ubuntu 18.04 (x86_64)
     $ make check
 
 ### Alarm clock
-
 - alarm : 호출한 프로세스를 정해진 시간 후에 다시 시작하는 커널 내부 함수
-- 핀토스에는 알람 기능이 Busy waiting을 이용하여 구현되어 있습니다.
+- 기존 핀토스에서는 알람 기능이 Busy waiting을 이용하여 구현되어 있습니다.
 - busy waiting은 스레드가 CPU를 점유하면서 대기하고 있는 상태로, 권한을 얻을 때까지 확인하는 것을 의미합니다. 이 경우 CPU의 자원을 불필요하게 낭비하기 때문에 좋지 않은 쓰레드 동기화 방식입니다.
 - 이 알람 기능을 sleep/wake up을 이용하여 재구현 하는 것이 목표입니다.
 
@@ -35,10 +34,9 @@ AWS EC2 Ubuntu 18.04 (x86_64)
 
 ### Priority Scheduling
 
-- 핀토스의 스케줄러는 라운드 로빈으로 구현되어 있습니다. 
+- 기존 핀토스의 스케줄러는 라운드 로빈으로 구현되어 있습니다. 
 - 라운드 로빈 스케줄링은 시분할 시스템을 위해 설계된 선점형 스케줄링의 하나로서, 우선순위와 관련 없이 시간 순서대로 CPU를 할당하는 방식입니다.
-- 즉, 기존 핀토스에서는 ready list에 삽입된 순으로 thread가 CPU를 점유하게 됩니다. 이 경우 우선순위가 낮거나 하나의 CPU 사용 시간이 길어지면 자원 낭비와 성능이 저하될 우려가 있습니다. 
-- 따라서 이를 우선순위를 고려하여 스케줄링 하도록 수정하는 것이 목표입니다.
+- 이를 우선순위를 고려하여 스케줄링 하도록 수정하는 것이 목표입니다.
 
 ![스크린샷 2023-06-05 오전 11 13 56](https://github.com/Riudiu/pintos/assets/86466976/1877700d-2692-4bdd-9634-a93b4bf97bc9)
 
@@ -48,21 +46,6 @@ AWS EC2 Ubuntu 18.04 (x86_64)
 
 - 우선 순위 스케줄링의 문제점 중 하나는 'priority inversion'입니다. 
 - H, M, L 을 각각 높은, 중간, 낮은 우선 순위의 thread라 해봅시다. H가 L을 기다려야 하고(예를 들면, L이 lock을 걸은 경우) M은 ready list에 있다면, L이 CPU 이용 시간을 얻지 못하기 때문에, H도 절대 CPU를 사용하지 못하게 될 것입니다.
-- Priority donation을 구현하여 lock을 보유한 낮은 우선순위의 thread에게 자신의 우선순위를 기부하도록 합니다. 그리고 lock이 풀리면 기부한 우선순위를 다시 가져오도록 구현하는 것이 목표입니다.
+- Priority donation 구현하여 lock을 보유한 낮은 우선순위의 thread에게 자신의 우선순위를 기부하도록 합니다. 그리고 lock이 풀리면 기부한 우선순위를 다시 가져오도록 구현하는 것이 목표입니다.
 
 ![스크린샷 2023-06-05 오전 11 23 59](https://github.com/Riudiu/pintos/assets/86466976/993b2e64-4ddc-4b04-a514-ad12890639a7)
-
-## PJ2. User Programs
-
-제대로 동작되는지 테스트 결과 확인
-
-    $ source ./activate
-    $ cd userprog
-    $ make check
-
-### Argument Passing
-
-- 핀토스는 프로그램과 인자(argument)를 구분하지 못하는 구조를 가지고 있습니다. 
-- 프로그램 이름과 인자를 구분하여(받은 문자열을 parsing후 argv, argc에 저장) 스택에 저장하고, 인자를 응용 프로그램에 전달하는 기능을 구현하는 것이 목표입니다. 
-
-![image](https://github.com/Riudiu/pintos/assets/86466976/6e7faaf8-fe14-49d1-b242-748cdb9c08a3)
--- userprog/process.c
+++ userprog/process.c
@@ -176,75 +176,19 @@ process_exec (void *f_name) {
 	/* We first kill the current context */
 	process_cleanup ();
 
-	/// for argument parsing
-	char *argv[64]; 	// argument 배열
-	int argc = 0;		// argument 개수
-
-	char *token;		// 실제 리턴 받을 토큰
-	char *save_ptr;		// 분리된 문자열 중 남는 부분의 시작주소
-	token = strtok_r(file_name, " ", &save_ptr);
-	while (token != NULL) {
-		argv[argc] = token;
-		token = strtok_r(NULL, " ", &save_ptr);
-		argc++;
-	}
-
 	/* And then load the binary */
 	success = load (file_name, &_if);
 
-	 /* If load failed, quit. */
-    if (!success)
-    {
-        palloc_free_page(file_name);
-        return -1;
-    }
-
-	/// insert a Factor in the User Stack
-	void **rspp = &_if.rsp;
-	argument_stack(argv, argc, rspp);
-	_if.R.rdi = argc;
-	_if.R.rsi = (uint64_t)*rspp + sizeof(void *);
-
-	hex_dump(_if.rsp, _if.rsp, USER_STACK - (uint64_t)*rspp, true);
+	/* If load failed, quit. */
 	palloc_free_page (file_name);
+	if (!success)
+		return -1;
 
 	/* Start switched process. */
 	do_iret (&_if);
 	NOT_REACHED ();
 }
 
-void argument_stack(char **argv, int argc, void **rsp) {
-	// Save argument strings (character by character)
-	for (int i = argc - 1; i >= 0; i--) {
-		int argv_len = strlen(argv[i]);
-		for (int j = argv_len; j >= 0; j--) {
-			char argv_char = argv[i][j];
-			(*rsp)--;
-			**(char **)rsp = argv_char; // 1 byte
-		}
-		argv[i] = *(char **)rsp; 		// insert rsp address in list
-	}
-
-	// Word-align padding
-	int pad = (int)*rsp % 8;
-	for (int k = 0; k < pad; k++) {
-		(*rsp)--;
-		**(uint8_t **)rsp = 0;
-	}
-
-	// Pointers to the argument strings
-	(*rsp) -= 8;
-	**(char ***)rsp = 0;
-
-	for (int i = argc - 1; i >= 0; i--) {
-		(*rsp) -= 8;
-		**(char ***)rsp = argv[i];
-	}
-
-	// Return address
-	(*rsp) -= 8;
-	**(void ***)rsp = 0;
-}
 
 /* Waits for thread TID to die and returns its exit status.  If
  * it was terminated by the kernel (i.e. killed due to an
@@ -257,7 +201,9 @@ void argument_stack(char **argv, int argc, void **rsp) {
  * does nothing. */
 int
 process_wait (tid_t child_tid UNUSED) {
-	for (int i = 0; i < 100000000; i++) {}
+	/* XXX: Hint) The pintos exit if process_wait (initd), we recommend you
+	 * XXX:       to add infinite loop here before
+	 * XXX:       implementing the process_wait. */
 	return -1;
 }
 
